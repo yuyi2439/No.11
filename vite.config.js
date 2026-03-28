76db9bedb2af36478428dae38914e6984eef76bc
yuyi2439 <yuyi2439@qq.com>
2026-03-28 22:51:50 +0800

    singalfile

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  plugins: [
+    svelte(),
+    viteSingleFile()
+  ],
 
-  base: process.env.BUILD_FOR_GITHUB == 'true'
-      ? '/No.11/'
-      : './',
+  // 关键：相对路径，让双击能打开
+  base: './',
+
+  build: {
+    // 禁止代码分割，全部打进一个文件
+    rollupOptions: {
+      inlineDynamicImports: true,
+      output: {
+        // 打包成浏览器直接运行的格式
+        format: 'iife'
+      }
+    }
+  }
 })

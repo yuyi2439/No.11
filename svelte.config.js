3505e4b6f8d7e0fa803b2d52fddc4f030afa8fc5
yuyi2439 <yuyi2439@qq.com>
2026-03-28 11:54:29 +0800

    fix github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+
+  vite: {
+    base: process.env.BUILD_FOR_GITHUB === 'true'
+      ? '/No.11/'
+      : '/'
+  }
 }
